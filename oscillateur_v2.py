a01228ba514ab26acb033cca517b0dfc59a67781
zazbone <coczaz@gmail.com>
2020-02-21 16:20:10 +0100

    dfghj

--- oscillateur_v2.py
+++ oscillateur_v2.py
@@ -6,15 +6,30 @@ class Pendulum:
     def __init__(self):
         self.time = 0
         self.angle = np.pi / 3
-        self.radius = 0.1
+        self.radius = 1
+        self.k = 0.1
+        self.l
+        self.omega = 2*np.pi
+        self.omega_carre = self.omega * self.omega
         self.angular_acc = 0
         self.angular_speed = 0
+        self.m_g = 0.02 * g
+
+    def L(self):
+        return self.l + self.k * np.cos(self.omega * self.time)
+
+    def L_prim(self):
+        return self.l - self.omega * self.k * np.sin(self.omega * self.time)
+
+    def L_prim2(self):
+        return self.l + self.omega**2 * self.k * np.cos(self.omega * self.time)
 
     def calc_acc(self):
+        acc = -self.omega_carre
         if self.angle > 0:
-            self.angular_acc = (-g * np.sin(self.angle)) / self.radius
+            self.angular_acc = acc
         else:
-            self.angular_acc = (g * np.sin(self.angle)) / self.radius
+            self.angular_acc = -acc
 
     def __iter__(self):
         return self
@@ -23,7 +38,7 @@ class Pendulum:
         if self.time <= T:
             self.calc_acc()
             self.angular_speed += self.angular_acc * dt
-            self.angular_speed *= 1 - 0.05 * dt
+            self.angular_speed *= 1 - 0.00 * dt
             self.angle += self.angular_speed
             self.time += dt
             return self.angle, self.angular_speed, self.angular_acc
@@ -32,6 +47,7 @@ class Pendulum:
 
 
 def main():
+
     pendulum = Pendulum()
     time_axes = np.arange(0, T, dt)
     mouvement = iter(pendulum)
@@ -44,15 +60,17 @@ def main():
         angle.append(i[0])
         speed.append(i[1])
         acc.append(i[2])
-        x_coor.append(pendulum.radius * np.sin(i[0]))
-        y_coor.append(-pendulum.radius * np.cos(i[0]))
-    plt.plot(angle, speed)
+        x_coor.append((pendulum.radius + pendulum.L()) * np.sin(i[0]))
+        y_coor.append(-(pendulum.radius + pendulum.L()) * np.cos(i[0]))
+    #plt.plot(angle, speed)
     #plt.plot(time_axes, angle)
     #plt.plot(time_axes, speed)
     #plt.plot(time_axes, acc)
     #plt.plot(time_axes, x_coor)
     #plt.plot(time_axes, y_coor)
-    #plt.plot(x_coor, y_coor)
+    plt.plot(time_axes, x_coor)
+    plt.xlabel("t")
+    plt.ylabel("x")
     plt.show()
 
 
