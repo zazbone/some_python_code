95a40673f6292299cf7f816c872c30e885c66ea4
zazbone <coczaz@gmail.com>
2020-03-04 21:51:54 +0100

    correction

--- oscillateur_v2.py
+++ oscillateur_v2.py
@@ -4,35 +4,39 @@ import matplotlib.pyplot as plt
 
 class Pendulum:
     def __init__(self):
-        self.time = 0
-        self.angle = np.pi / 3
-        self.angular_acc = 0
+        self.time = 0  # Paramètres du système
+        self.angle = np.pi / 3.5
         self.angular_speed = 0
+        self.angular_acc = 0
         self.k = 0.02
-        self.l = 0.1
-        self.omega = np.pi
-        self.omega_carre = self.omega * self.omega
-        self.m_g = 0.02 * g
+        self.l = 0.4
+        self.omega = np.pi * 3
 
-    def L(self):
+    def L(self):  # Renvoie la longueur L(t)
         return self.l + self.k * np.cos(self.omega * self.time)
 
-    def L_prim(self):
+    def L_prim(self): # Renvoie la dérivée de L(t)
         return -self.omega * self.k * np.sin(self.omega * self.time)
 
-    def calc_acc(self):
-        self.angular_acc = -(2 * self.L_prim() / self.L() * self.angular_speed + self.m_g / self.L() * np.sin(self.angle))
+    def calc_acc(self):  # Modifie l'accélération
+        self.angular_acc = -((2*self.L_prim() * self.angular_speed)/self.L()\
+         + (g * np.sin(self.angle)) / self.L())
 
-    def __iter__(self):
+    def __iter__(self):  # Définition du générateur de l'itérable
         return self
 
     def __next__(self):
+        """
+        Calcule et renvoie l'angle à chaque instant t de 0 a T
+            sous forme d'itérable.
+        Renvoie le temps et l'angle.
+        """
         if self.time <= T:
             self.calc_acc()
             self.angular_speed += self.angular_acc * dt
             self.angle += self.angular_speed * dt
             self.time += dt
-            return self.time, self.angular_acc, self.angular_speed, self.angle, self.L()
+            return self.time, self.angle
         else:
             raise StopIteration
 
@@ -43,9 +47,8 @@ def main():
     teta = []
     for i in iter(pendulum):
         time.append(i[0])
-        teta.append(i[3])
+        teta.append(i[1])
     plt.plot(time, teta)
-    plt.title("Angle Θ fonction du temps")
     plt.xlabel("Temps (s)")
     plt.ylabel("Angle Θ (rad)")
     plt.show()
@@ -53,6 +56,6 @@ def main():
 
 if __name__ == "__main__":
     dt = 0.001
-    T = 100
+    T = 30  # constantes globales
     g = 9.81
     main()
