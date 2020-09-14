54d8899fe6f85faa96d9040c4f3362e6ee510408
Yann Querlioz <coczaz@gmail.com>
2020-09-14 16:59:08 +0200

    before cours

--- hell/display_half_string.py
+++ hell/display_half_string.py
@@ -2,3 +2,5 @@ print(input("Give à String: ")[::2])
 
 # Given a string, display only those characters which are present at an even index number.
 # From: https://pynative.com/python-basic-exercise-for-beginners/
+
+# NB: I'm not sure I understood the instructions correctly
---
+++ hell/keep_multiple_of_five.py
@@ -0,0 +1,1 @@
+print(*filter(lambda x: not x % 5,[int(i) for i in input("Give a list of int: ").split(",")]))
