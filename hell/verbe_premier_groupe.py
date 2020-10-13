7c4ddbcbad0e8646d7b1c963bd8b5681fd2219a3
zazbone <coczaz@gmail.com>
2020-10-13 14:34:20 +0200

    more fire

---
+++ hell/verbe_premier_groupe.py
@@ -0,0 +1,5 @@
+print((lambda x: x[0] in "aeiouy" and x[-2:] == "er")(input("Donnez un verbe à l'infinitif: ")))
+
+# Vérifier si un verbe à l'infinitif est du premier groupe et commence pour une voyelle
+
+# Issu d'un exercice de lycée presenté sur NaN
