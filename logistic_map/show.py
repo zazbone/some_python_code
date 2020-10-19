c3d3284b4653e0cf3d8d85fc58f9d0bb2245646b
zazbone <coczaz@gmail.com>
2020-10-19 12:42:59 +0200

    Error in test value

--- logistic_map/show.py
+++ logistic_map/show.py
@@ -5,8 +5,10 @@ import numpy as np
 from .suite import log_suite_gen
 
 
-def generation_plot(k=3.85, init_value=0.9, n=100):
+def generation_plot(k=3.45, init_value=0.9, n=100):
     n_axes = np.arange(101)
     u_axes = np.array(list(log_suite_gen(k, init_value, n_max=n)))
+    for n, u in zip(n_axes, u_axes):
+        print(f"Gen {n} u={u}")
     plt.plot(n_axes, u_axes)
     plt.show()
