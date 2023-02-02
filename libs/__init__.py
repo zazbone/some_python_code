26fe6548ad5469040f4a950e23187aecaef7b384
zazbone <coczaz@gmail.com>
2023-02-02 19:00:24 +0100

    setup

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,1 +1,13 @@
 """"""
+import numpy as np
+
+
+PI2_4 = 4 * np.pi ** 2
+def laplacian(xy):
+    """
+    Compute laplacian of a 2D numpy array using spectral differentiation
+    Y is row index, x column index
+    """
+    ny, nx = xy.shape
+    XY = np.fft.rfft2(xy, xy.shape)
+    ik = - PI2_4 * np.fft.fftfreq(nx)
